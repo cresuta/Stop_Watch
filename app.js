bb2ffb7cf10cdfd9ced0a428c65bbb649d59d9de
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-03-30 22:08:08 -0400

    basic html markup, css styling, variables added to js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const start = document.querySelector('.btn-start');
+const stop = document.querySelector('.btn-stop');
+const reset = document.querySelector('.btn-reset');
+const timer = document.querySelector('.timer');
+
+start.addEventListener('click', () => {
+  
+})
